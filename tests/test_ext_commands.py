63bf35a3c1042de619ed3b46ad8b8110dac1eb8c
slarse <slarse@kth.se>
2020-06-29 18:00:52 +0200

    Refactor fake repo setup to actually be a repo

--- tests/test_ext_commands.py
+++ tests/test_ext_commands.py
@@ -2,6 +2,7 @@ import collections
 import pathlib
 
 import pytest
+import git
 import repobee
 
 import testhelpers
@@ -10,7 +11,7 @@ _FileInfo = collections.namedtuple(
     "_FileInfo", "expected_text abspath relpath".split()
 )
 _FakeRepoInfo = collections.namedtuple(
-    "_FakeRepoInfo", "root_path file_list_path file_infos"
+    "_FakeRepoInfo", "repo path file_list_path file_infos"
 )
 
 
@@ -35,12 +36,15 @@ def test_sanitize_valid_file(sanitizer_config, tmpdir):
     assert outfile.read_text(encoding="utf8") == out_text
 
 
-def test_sanitize_repo_dictate_mode(sanitizer_config, fake_repo):
-    """Test sanitize repo with a list of files to sanitize."""
+def test_sanitize_repo_dictate_mode_dry_run(sanitizer_config, fake_repo):
+    """Test sanitize repo with a list of files to sanitize in current working
+    tree of the repository.
+    """
     repobee.main(
         f"repobee --config-file {sanitizer_config} sanitize-repo "
+        "--dry-run "
         f"--file-list {fake_repo.file_list_path} "
-        f"--repo-root {fake_repo.root_path}".split()
+        f"--repo-root {fake_repo.path}".split()
     )
 
     asserted = False
@@ -53,25 +57,32 @@ def test_sanitize_repo_dictate_mode(sanitizer_config, fake_repo):
 @pytest.fixture
 def fake_repo(tmpdir) -> _FakeRepoInfo:
     """Setup a fake repository to sanitize."""
-    fake_repo = pathlib.Path(tmpdir)
+    fake_repo_path = pathlib.Path(tmpdir)
     file_infos = []
     for raw_case, id_ in zip(*testhelpers.generate_valid_test_cases()):
         input_text, expected_output_text = raw_case
-        output_path = fake_repo / id_
+        output_path = fake_repo_path / id_
         output_path.parent.mkdir(parents=True)
         output_path.write_text(input_text)
         file_infos.append(
             _FileInfo(
                 expected_text=expected_output_text,
-                abspath=output_path,
+                abspath=fake_repo_path / id_,
                 relpath=pathlib.Path(id_),
             )
         )
 
-    file_list = fake_repo / "file_list.txt"
+    fake_repo = git.Repo.init(tmpdir)
+    fake_repo.git.add(".")
+    fake_repo.git.commit("-m", "'Initial commit'")
+
+    file_list = fake_repo_path / "file_list.txt"
     file_list_content = "\n".join([str(c.relpath) for c in file_infos])
     file_list.write_text(file_list_content)
 
     return _FakeRepoInfo(
-        root_path=fake_repo, file_list_path=file_list, file_infos=file_infos,
+        repo=fake_repo,
+        path=fake_repo_path,
+        file_list_path=file_list,
+        file_infos=file_infos,
     )
